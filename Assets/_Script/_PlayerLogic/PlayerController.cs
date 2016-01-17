83d651b7a52b266e6c0b91c5c86788ca2ae8ae65
Yusuf Afandi <afandi.yusuf.04@gmail.com>
2016-01-17 21:15:36 +0700

    init commit and add player movement

---
+++ Assets/_Script/_PlayerLogic/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float Speed;
+	Rigidbody2D thisRig;
+
+	// Use this for initialization
+	void Start () {
+		thisRig = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal") * Speed;
+		float v = Input.GetAxis ("Vertical") * Speed;
+		thisRig.velocity = new Vector2 (h, v);
+	}
+}
